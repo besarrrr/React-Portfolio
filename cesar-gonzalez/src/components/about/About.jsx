590d9655a6aae36f95d8b79e783cc52ade9a21b6
Cesar Gonzalez <gcesar13@yahoo.com>
2022-09-24 21:22:30 -0600

    about me section

--- cesar-gonzalez/src/components/about/About.jsx
+++ cesar-gonzalez/src/components/about/About.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
 import './about.css'
+import ME2 from '../../assets/aboutme.png'
+import {FaAward} from 'react-icons/fa'
+import {AiFillFolderOpen} from 'react-icons/ai'
+import {MdSportsGolf} from 'react-icons/md'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id="about">
+      <h2>About Me</h2>
+
+      <div className="container about__container"> 
+      <div className="about__me">
+      <div className="about__me-image">
+        <img src={ME2} alt="me"/>
+      </div>
+      </div>
+
+      <div className="about__content">
+        <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small> Recent Graduate of UT Austin Coding Bootcamp</small>
+            </article>
+
+            <article className="about__card">
+              <MdSportsGolf className='about__icon'/>
+              <h5>Hobbies</h5>
+              <small> Ski, Golf, Boardgames</small>
+            </article>
+
+            <article className="about__card">
+              <AiFillFolderOpen className='about__icon'/>
+              <h5>Projects</h5>
+              <small> Open to anything!</small>
+            </article>
+        </div>
+
+        <p>
+        I am currently in full-stack software
+        engineering bootcamp through the University of Texas at Austin and I am open for work. I am excited about applying what I have
+        learned in the class room in a real world setting. In any role, I look for an environment that is collaborative and fosters continual learning. I am
+        looking to learn more in a diverse technology ecosystem within a highly collaborative applications development
+        team. I am an ambitious problem solver leveraging a background in customer service to
+        deliver client satisfaction through leadership and technical know-how. 
+        </p>
+
+        <a href="#contact" className="btn btn-primary"> Connect</a>
+
+      </div>
+      </div>
+    </section>
   )
 }
 
--- cesar-gonzalez/src/components/nav/Nav.jsx
+++ cesar-gonzalez/src/components/nav/Nav.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './nav.css'
+import {AiTwotoneHome} from 'react-icons/ai'
+import{FaUserAstronaut, FaAmericanSignLanguageInterpreting} from 'react-icons/fa'
+import {GiBookshelf} from 'react-icons/gi'
+import {BiMessageDetail} from 'react-icons/bi'
+import {useState} from 'react';
 
-const Nav = () => {
+
+const Nav = () => { 
+  const [activeNav, setActiveNav] = useState('#')
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiTwotoneHome/></a>
+      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserAstronaut/></a>
+      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><FaAmericanSignLanguageInterpreting/></a>
+      <a href="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><GiBookshelf/></a>
+      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageDetail/></a>
+
+    </nav>
   )
 }
 
