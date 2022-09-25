ed61f4c6e0fc7ec869c77f112a7cb50f089e3f79
Cesar Gonzalez <gcesar13@yahoo.com>
2022-09-25 10:50:43 -0600

    portfolio section

--- cesar-gonzalez/src/components/experience/Experience.jsx
+++ cesar-gonzalez/src/components/experience/Experience.jsx
@@ -3,7 +3,35 @@ import './experience.css'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      
+      <h2>My Skills and Knowledge</h2>
+      <div className="container experience__container">
+          <div className="frontend">
+              <h2>Frontend</h2>
+              <div className="experience__content">
+                <article className='experience__details'>
+                  <h4>
+                  HTML5, CSS, JQuery, Javascript, Bootstrap, AJAX, SASS
+                   </h4>
+              </article>
+              </div>
+          </div>
+
+          <div className="backend">
+          <h2>Backend</h2>
+          <div className="experience__content">
+                <article className='experience__details'>
+                  <h4>
+                  MySQL, MongoDB, Express, ReactJS, Node, Handlebars, Firebase, AWS, Webpack, Gulp
+                   </h4>
+              </article>
+              </div>
+
+          </div>
+
+      </div>
+    </section>
   )
 }
 
--- cesar-gonzalez/src/components/portfolio/Portfolio.jsx
+++ cesar-gonzalez/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/img1.png'
+import IMG2 from '../../assets/img2.png'
+
+const data = [
+  {
+    id:1,
+    image: IMG1,
+    title: 'Dragons and Chats',
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+  {
+    id:2,
+    image: IMG2,
+    title: 'Foodle',
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+  {
+    id:3,
+    image: IMG1,
+    title: 'Dragons and Chats',
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+  {
+    id:4,
+    image: IMG1,
+    title: 'Dragons and Chats',
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+  {
+    id:5,
+    image: IMG1,
+    title: 'Dragons and Chats',
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+  {
+    id:6,
+    image: IMG1,
+    title: 'Dragons and Chats',
+    github: "https://github.com",
+    demo: "https://github.com"
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+       {
+        data.map(({id, image, title, github, demo}) => {
+          return (
+
+            <article key={id} className="portfolio__item">
+            <div className="portfolio__item-image">
+              <img src={image} alt={title}/>
+            </div>
+            <h3> {title}</h3>
+            <div className="portfolio__item-cta">
+            <a href={github} className="btn" target="_blank"> Github</a>
+            <a href={demo} className="btn btn-primary" target="_blank"> Website!</a>
+            </div>
+            </article>
+          )
+
+        })
+       }
+        
+      </div>
+    </section>
   )
 }
 
